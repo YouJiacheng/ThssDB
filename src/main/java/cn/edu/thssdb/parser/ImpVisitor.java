479486c82c1304fdecd5af2582afb32d5444b717
YouJiacheng <1503679330@qq.com>
2022-06-24 01:53:08 +0800

    feat: create table stmt
    WIP: show meta stmt(show table tableName)

--- src/main/java/cn/edu/thssdb/parser/ImpVisitor.java
+++ src/main/java/cn/edu/thssdb/parser/ImpVisitor.java
@@ -5,22 +5,29 @@ package cn.edu.thssdb.parser;
 
 import cn.edu.thssdb.exception.DatabaseNotExistException;
 import cn.edu.thssdb.query.QueryResult;
+import cn.edu.thssdb.schema.Column;
 import cn.edu.thssdb.schema.Database;
 import cn.edu.thssdb.schema.Manager;
+import cn.edu.thssdb.type.ColumnType;
+import org.antlr.v4.runtime.RuleContext;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * When use SQL sentence, e.g., "SELECT avg(A) FROM TableX;"
  * the parser will generate a grammar tree according to the rules defined in SQL.g4.
  * The corresponding terms, e.g., "select_stmt" is a root of the parser tree, given the rules
  * "select_stmt :
- *     K_SELECT ( K_DISTINCT | K_ALL )? result_column ( ',' result_column )*
- *         K_FROM table_query ( ',' table_query )* ( K_WHERE multiple_condition )? ;"
- *
+ * K_SELECT ( K_DISTINCT | K_ALL )? result_column ( ',' result_column )*
+ * K_FROM table_query ( ',' table_query )* ( K_WHERE multiple_condition )? ;"
+ * <p>
  * This class "ImpVisit" is used to convert a tree rooted at e.g. "select_stmt"
  * into the collection of tuples inside the database.
- *
+ * <p>
  * We give you a few examples to convert the tree, including create/drop/quit.
  * You need to finish the codes for parsing the other rooted trees marked TODO.
  */
@@ -37,7 +44,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
 
     private Database GetCurrentDB() {
         Database currentDB = manager.getCurrentDatabase();
-        if(currentDB == null) {
+        if (currentDB == null) {
             throw new DatabaseNotExistException();
         }
         return currentDB;
@@ -46,7 +53,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
     public QueryResult visitSql_stmt(SQLParser.Sql_stmtContext ctx) {
         if (ctx.create_db_stmt() != null) return new QueryResult(visitCreate_db_stmt(ctx.create_db_stmt()));
         if (ctx.drop_db_stmt() != null) return new QueryResult(visitDrop_db_stmt(ctx.drop_db_stmt()));
-        if (ctx.use_db_stmt() != null)  return new QueryResult(visitUse_db_stmt(ctx.use_db_stmt()));
+        if (ctx.use_db_stmt() != null) return new QueryResult(visitUse_db_stmt(ctx.use_db_stmt()));
         if (ctx.create_table_stmt() != null) return new QueryResult(visitCreate_table_stmt(ctx.create_table_stmt()));
         if (ctx.drop_table_stmt() != null) return new QueryResult(visitDrop_table_stmt(ctx.drop_table_stmt()));
         if (ctx.insert_stmt() != null) return new QueryResult(visitInsert_stmt(ctx.insert_stmt()));
@@ -54,6 +61,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
         if (ctx.update_stmt() != null) return new QueryResult(visitUpdate_stmt(ctx.update_stmt()));
         if (ctx.select_stmt() != null) return visitSelect_stmt(ctx.select_stmt());
         if (ctx.quit_stmt() != null) return new QueryResult(visitQuit_stmt(ctx.quit_stmt()));
+        if (ctx.show_meta_stmt() != null) return new QueryResult(visitShow_meta_stmt(ctx.show_meta_stmt()));
         return null;
     }
 
@@ -68,7 +76,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
     }
 
     /**
-     创建数据库
+     * 创建数据库
      */
     @Override
     public String visitCreate_db_stmt(SQLParser.Create_db_stmtContext ctx) {
@@ -82,7 +90,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
     }
 
     /**
-     删除数据库
+     * 删除数据库
      */
     @Override
     public String visitDrop_db_stmt(SQLParser.Drop_db_stmtContext ctx) {
@@ -95,7 +103,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
     }
 
     /**
-     切换数据库
+     * 切换数据库
      */
     @Override
     public String visitUse_db_stmt(SQLParser.Use_db_stmtContext ctx) {
@@ -108,7 +116,7 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
     }
 
     /**
-     删除表格
+     * 删除表格
      */
     @Override
     public String visitDrop_table_stmt(SQLParser.Drop_table_stmtContext ctx) {
@@ -121,42 +129,110 @@ public class ImpVisitor extends SQLBaseVisitor<Object> {
     }
 
     /**
-     * TODO
-     创建表格
+     * 创建表格
      */
+    private ColumnType getColumnType(SQLParser.Type_nameContext ctx) {
+        if (ctx.T_INT() != null) return ColumnType.INT;
+        if (ctx.T_LONG() != null) return ColumnType.LONG;
+        if (ctx.T_FLOAT() != null) return ColumnType.FLOAT;
+        if (ctx.T_DOUBLE() != null) return ColumnType.DOUBLE;
+        if (ctx.T_STRING() != null) return ColumnType.STRING;
+        return null;
+    }
+
+    private int getMaxLength(SQLParser.Type_nameContext ctx) {
+        var maxLen = ctx.NUMERIC_LITERAL();
+        if (maxLen != null) return Integer.parseInt(maxLen.getText());
+        return 0;
+    }
+
+    private boolean getNotNull(SQLParser.Column_defContext ctx) {
+        var constraint = ctx.column_constraint(0);
+        return (constraint != null) && (constraint.K_NULL() != null);
+    }
+
+    private List<String> getPrimaryKeys(SQLParser.Create_table_stmtContext ctx) {
+        var constraint = ctx.table_constraint();
+        if (constraint != null) {
+            return constraint.column_name().stream().map(RuleContext::getText).toList();
+        }
+        return Collections.emptyList();
+    }
+
     @Override
-    public String visitCreate_table_stmt(SQLParser.Create_table_stmtContext ctx) {return null;}
+    public String visitCreate_table_stmt(SQLParser.Create_table_stmtContext ctx) {
+        try {
+            var primaryKeys = getPrimaryKeys(ctx);
+            var columns = ctx.column_def().stream().map(
+                    it -> {
+                        var name = it.column_name().getText();
+                        var type = getColumnType(it.type_name());
+                        var maxLen = getMaxLength(it.type_name());
+                        var notNull = getNotNull(it);
+                        if (primaryKeys.contains(name)) {
+                            return new Column(name, type, 1, notNull, maxLen);
+                        }
+                        return new Column(name, type, 0, notNull, maxLen);
+                    }
+            ).toArray(Column[]::new);
+            GetCurrentDB().create(ctx.table_name().getText(), columns);
+            return "Create table " + ctx.table_name().getText() + ".";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 
     /**
      * TODO
-     表格项插入
+     * 显示表格
      */
-    @Override
-    public String visitInsert_stmt(SQLParser.Insert_stmtContext ctx) {return null;}
+    public String visitShow_meta_stmt(SQLParser.Show_meta_stmtContext ctx) {
+        try {
+            var table = GetCurrentDB().get(ctx.table_name().getText());
+            return table.tableName; // TODO
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 
     /**
      * TODO
-     表格项删除
+     * 表格项插入
      */
     @Override
-    public String visitDelete_stmt(SQLParser.Delete_stmtContext ctx) {return null;}
+    public String visitInsert_stmt(SQLParser.Insert_stmtContext ctx) {
+        return null;
+    }
 
     /**
      * TODO
-     表格项更新
+     * 表格项删除
      */
     @Override
-    public String visitUpdate_stmt(SQLParser.Update_stmtContext ctx) {return null;}
+    public String visitDelete_stmt(SQLParser.Delete_stmtContext ctx) {
+        return null;
+    }
 
     /**
      * TODO
-     表格项查询
+     * 表格项更新
      */
     @Override
-    public QueryResult visitSelect_stmt(SQLParser.Select_stmtContext ctx) {return null;}
+    public String visitUpdate_stmt(SQLParser.Update_stmtContext ctx) {
+        return null;
+    }
 
     /**
-     退出
+     * TODO
+     * 表格项查询
+     */
+    @Override
+    public QueryResult visitSelect_stmt(SQLParser.Select_stmtContext ctx) {
+        return null;
+    }
+
+    /**
+     * 退出
      */
     @Override
     public String visitQuit_stmt(SQLParser.Quit_stmtContext ctx) {
