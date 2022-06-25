ad78010331c06acfd04c03e8b34d15f5a1263308
YouJiacheng <1503679330@qq.com>
2022-06-26 00:43:00 +0800

    Fix: quit close transport

--- src/main/java/cn/edu/thssdb/client/Client.java
+++ src/main/java/cn/edu/thssdb/client/Client.java
@@ -23,233 +23,217 @@ import java.util.Scanner;
 
 public class Client {
 
-  private static final Logger logger = LoggerFactory.getLogger(Client.class);
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
 
-  static final String HOST_ARGS = "h";
-  static final String HOST_NAME = "host";
+    static final String HOST_ARGS = "h";
+    static final String HOST_NAME = "host";
 
-  static final String HELP_ARGS = "help";
-  static final String HELP_NAME = "help";
+    static final String HELP_ARGS = "help";
+    static final String HELP_NAME = "help";
 
-  static final String PORT_ARGS = "p";
-  static final String PORT_NAME = "port";
+    static final String PORT_ARGS = "p";
+    static final String PORT_NAME = "port";
 
-  private static final PrintStream SCREEN_PRINTER = new PrintStream(System.out);
-  private static final Scanner SCANNER = new Scanner(System.in);
+    private static final PrintStream SCREEN_PRINTER = new PrintStream(System.out);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
-  private static TTransport transport;
-  private static TProtocol protocol;
-  private static IService.Client client;
-  private static CommandLine commandLine;
+    private static IService.Client client;
 
-  private static String inTransaction = "";
+    private static String inTransaction = "";
 
-  private static long session = -1;
+    private static long session = -1;
 
-  public static void main(String[] args) {
-    commandLine = parseCmd(args);
-    if (commandLine.hasOption(HELP_ARGS)) {
-      showHelp();
-      return;
-    }
-    try {
-      echoStarting();
-      String host = commandLine.getOptionValue(HOST_ARGS, Global.DEFAULT_SERVER_HOST);
-      int port = Integer.parseInt(commandLine.getOptionValue(PORT_ARGS, String.valueOf(Global.DEFAULT_SERVER_PORT)));
-      boolean open = true;
-      while (true) {
-        print(Global.CLI_PREFIX + inTransaction + ">");
-        String msg = SCANNER.nextLine();
-        long startTime = System.currentTimeMillis();
-
-        transport = new TSocket(host, port);
-        transport.open();
-        protocol = new TBinaryProtocol(transport);
-        client = new IService.Client(protocol);
-
-        switch (msg.trim()) {
-          case Global.SHOW_TIME:
-            getTime();
-            break;
-          case Global.QUIT:
-            open = false;
-            break;
-          case Global.CONNECT:
-            connect();
-            break;
-          case Global.DISCONNECT:
-            disconnect();
-            break;
-          default:
-            execute(msg.trim());
-            break;
+    public static void main(String[] args) {
+        CommandLine commandLine = parseCmd(args);
+        if (commandLine.hasOption(HELP_ARGS)) {
+            showHelp();
+            return;
         }
-        long endTime = System.currentTimeMillis();
-        println("It costs " + (endTime - startTime) + " ms.");
-        if (!open) {
-          break;
+        try {
+            echoStarting();
+            String host = commandLine.getOptionValue(HOST_ARGS, Global.DEFAULT_SERVER_HOST);
+            int port = Integer.parseInt(commandLine.getOptionValue(PORT_ARGS, String.valueOf(Global.DEFAULT_SERVER_PORT)));
+            boolean open = true;
+            do {
+                print(Global.CLI_PREFIX + inTransaction + ">");
+                String msg = SCANNER.nextLine();
+                long startTime = System.currentTimeMillis();
+
+                TTransport transport = new TSocket(host, port);
+                transport.open();
+                TProtocol protocol = new TBinaryProtocol(transport);
+                client = new IService.Client(protocol);
+
+                switch (msg.trim()) {
+                    case Global.SHOW_TIME -> getTime();
+                    case Global.QUIT -> open = false;
+                    case Global.CONNECT -> connect();
+                    case Global.DISCONNECT -> disconnect();
+                    default -> execute(msg.trim());
+                }
+                long endTime = System.currentTimeMillis();
+                println("It costs " + (endTime - startTime) + " ms.");
+                transport.close();
+
+            } while (open);
+        } catch (TTransportException e) {
+            logger.error(e.getMessage());
         }
-        transport.close();
-      }
-    } catch (TTransportException e) {
-      logger.error(e.getMessage());
     }
-  }
 
-  private static void getTime() {
-    GetTimeReq req = new GetTimeReq();
-    try {
-      println(client.getTime(req).getTime());
-    } catch (TException e) {
-      logger.error(e.getMessage());
+    private static void getTime() {
+        GetTimeReq req = new GetTimeReq();
+        try {
+            println(client.getTime(req).getTime());
+        } catch (TException e) {
+            logger.error(e.getMessage());
+        }
     }
-  }
 
-  private static void connect() {
-    if (session >= 0) {
-      println("You're already connected.");
-      return;
-    }
+    private static void connect() {
+        if (session >= 0) {
+            println("You're already connected.");
+            return;
+        }
 //    print("Username: ");
 //    String username = SCANNER.nextLine();
 //    print("Password: ");
 //    String password = SCANNER.nextLine();
-    ConnectReq req = new ConnectReq("1", "1");
-    try {
-      ConnectResp resp = client.connect(req);
-      println(resp.toString());
-      if (resp.getStatus().code == Global.SUCCESS_CODE)
-        session = resp.getSessionId();
-    } catch (TException e) {
-      logger.error(e.getMessage());
-    }
-  }
-
-  private static void disconnect() {
-    if (session < 0) {
-      println("you're not connected. plz connect first.");
-      return;
-    }
-    DisconnetReq req = new DisconnetReq(session);
-    try {
-      DisconnetResp resp = client.disconnect(req);
-      println(resp.toString());
-      if (resp.getStatus().code == Global.SUCCESS_CODE)
-        session = -1;
-    } catch (TException e) {
-      logger.error(e.getMessage());
-    }
-  }
-
-  private static void execute(String msg) {
-    if (session < 0) {
-      println("you're not connected. plz connect first.");
-      return;
-    }
-    ExecuteStatementReq req = new ExecuteStatementReq();
-    req.setSessionId(session);
-    req.setStatement(msg);
-    try {
-      ExecuteStatementResp resp = client.executeStatement(req);
-      if (resp.getStatus().code == Global.FAILURE_CODE) {
-        println("Connection Failure!");
-        println(resp.getStatus().msg);
-      } else {
-        if (resp.isAbort) {
-          println("illegal SQL statement!");
+        ConnectReq req = new ConnectReq("1", "1");
+        try {
+            ConnectResp resp = client.connect(req);
+            println(resp.toString());
+            if (resp.getStatus().code == Global.SUCCESS_CODE)
+                session = resp.getSessionId();
+        } catch (TException e) {
+            logger.error(e.getMessage());
         }
-        else if (resp.isSetRowList()) {
-          StringBuilder column_str = new StringBuilder();
-          int column_size = resp.columnsList.size();
-          for (int i = 0; i < column_size; ++i) {
-            column_str.append(resp.columnsList.get(i));
-            if (i != column_size - 1) column_str.append(", ");
-          }
-          println(column_str.toString());
-          println("----------------------------------------------------------------");
+    }
 
-          for (List<String> row : resp.rowList) {
-            StringBuilder row_str = new StringBuilder();
-            for (int i = 0; i < column_size; ++i) {
-              row_str.append(row.get(i));
-              if (i != column_size - 1) row_str.append(", ");
+    private static void disconnect() {
+        if (session < 0) {
+            println("you're not connected. plz connect first.");
+            return;
+        }
+        DisconnetReq req = new DisconnetReq(session);
+        try {
+            DisconnetResp resp = client.disconnect(req);
+            println(resp.toString());
+            if (resp.getStatus().code == Global.SUCCESS_CODE)
+                session = -1;
+        } catch (TException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private static void execute(String msg) {
+        if (session < 0) {
+            println("you're not connected. plz connect first.");
+            return;
+        }
+        ExecuteStatementReq req = new ExecuteStatementReq();
+        req.setSessionId(session);
+        req.setStatement(msg);
+        try {
+            ExecuteStatementResp resp = client.executeStatement(req);
+            if (resp.getStatus().code == Global.FAILURE_CODE) {
+                println("Connection Failure!");
+                println(resp.getStatus().msg);
+            } else {
+                if (resp.isAbort) {
+                    println("illegal SQL statement!");
+                } else if (resp.isSetRowList()) {
+                    StringBuilder column_str = new StringBuilder();
+                    int column_size = resp.columnsList.size();
+                    for (int i = 0; i < column_size; ++i) {
+                        column_str.append(resp.columnsList.get(i));
+                        if (i != column_size - 1) column_str.append(", ");
+                    }
+                    println(column_str.toString());
+                    println("----------------------------------------------------------------");
+
+                    for (List<String> row : resp.rowList) {
+                        StringBuilder row_str = new StringBuilder();
+                        for (int i = 0; i < column_size; ++i) {
+                            row_str.append(row.get(i));
+                            if (i != column_size - 1) row_str.append(", ");
+                        }
+                        println(row_str.toString());
+                    }
+                } else {
+                    for (String column : resp.columnsList) {
+                        column = column.trim();
+                        if (column.equals("start transaction")) inTransaction = "(T)";
+                        else if (column.equals("commit transaction")) inTransaction = "";
+                        println(column);
+                    }
+                }
             }
-            println(row_str.toString());
-          }
-        } else {
-          for (String column : resp.columnsList) {
-            column = column.trim();
-            if (column.equals("start transaction")) inTransaction = "(T)";
-            else if (column.equals("commit transaction")) inTransaction = "";
-            println(column);
-          }
+        } catch (TException e) {
+            logger.error(e.getMessage());
         }
-      }
-    } catch (TException e) {
-      logger.error(e.getMessage());
     }
-  }
 
-  static Options createOptions() {
-    Options options = new Options();
-    options.addOption(Option.builder(HELP_ARGS)
-            .argName(HELP_NAME)
-            .desc("Display help information(optional)")
-            .hasArg(false)
-            .required(false)
-            .build()
-    );
-    options.addOption(Option.builder(HOST_ARGS)
-            .argName(HOST_NAME)
-            .desc("Host (optional, default 127.0.0.1)")
-            .hasArg(false)
-            .required(false)
-            .build()
-    );
-    options.addOption(Option.builder(PORT_ARGS)
-            .argName(PORT_NAME)
-            .desc("Port (optional, default 6667)")
-            .hasArg(false)
-            .required(false)
-            .build()
-    );
-    return options;
-  }
-
-  static CommandLine parseCmd(String[] args) {
-    Options options = createOptions();
-    CommandLineParser parser = new DefaultParser();
-    CommandLine cmd = null;
-    try {
-      cmd = parser.parse(options, args);
-    } catch (ParseException e) {
-      logger.error(e.getMessage());
-      println("Invalid command line argument!");
-      System.exit(-1);
+    static Options createOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder(HELP_ARGS)
+                .argName(HELP_NAME)
+                .desc("Display help information(optional)")
+                .hasArg(false)
+                .required(false)
+                .build()
+        );
+        options.addOption(Option.builder(HOST_ARGS)
+                .argName(HOST_NAME)
+                .desc("Host (optional, default 127.0.0.1)")
+                .hasArg(false)
+                .required(false)
+                .build()
+        );
+        options.addOption(Option.builder(PORT_ARGS)
+                .argName(PORT_NAME)
+                .desc("Port (optional, default 6667)")
+                .hasArg(false)
+                .required(false)
+                .build()
+        );
+        return options;
     }
-    return cmd;
-  }
 
-  static void showHelp() {
-    // TODO
-    println("DO IT YOURSELF");
-  }
+    static CommandLine parseCmd(String[] args) {
+        Options options = createOptions();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            logger.error(e.getMessage());
+            println("Invalid command line argument!");
+            System.exit(-1);
+        }
+        return cmd;
+    }
 
-  static void echoStarting() {
-    println("----------------------");
-    println("Starting ThssDB Client");
-    println("----------------------");
-  }
+    static void showHelp() {
+        // TODO
+        println("DO IT YOURSELF");
+    }
 
-  static void print(String msg) {
-    SCREEN_PRINTER.print(msg);
-  }
+    static void echoStarting() {
+        println("----------------------");
+        println("Starting ThssDB Client");
+        println("----------------------");
+    }
 
-  static void println() {
-    SCREEN_PRINTER.println();
-  }
+    static void print(String msg) {
+        SCREEN_PRINTER.print(msg);
+    }
 
-  static void println(String msg) {
-    SCREEN_PRINTER.println(msg);
-  }
+    static void println() {
+        SCREEN_PRINTER.println();
+    }
+
+    static void println(String msg) {
+        SCREEN_PRINTER.println(msg);
+    }
 }
--- src/main/java/cn/edu/thssdb/server/ThssDB.java
+++ src/main/java/cn/edu/thssdb/server/ThssDB.java
@@ -16,15 +16,6 @@ public class ThssDB {
 
     private static final Logger logger = LoggerFactory.getLogger(ThssDB.class);
 
-    private static IServiceHandler handler;
-    private static IService.Processor<IServiceHandler> processor;
-    private static TServerSocket transport;
-    // private static TServer server;
-    private static  TThreadPoolServer mul_sever;
-
-
-    private Manager manager;
-
     public static ThssDB getInstance() {
         return ThssDBHolder.INSTANCE;
     }
@@ -35,19 +26,18 @@ public class ThssDB {
     }
 
     private void start() {
-        handler = new IServiceHandler();
-        processor = new IService.Processor<>(handler);
+        IServiceHandler handler = new IServiceHandler();
+        var processor = new IService.Processor<>(handler);
         Runnable setup = () -> setUp(processor);
         new Thread(setup).start();
     }
 
     private static void setUp(IService.Processor<IServiceHandler> processor) {
         try {
-            transport = new TServerSocket(Global.DEFAULT_SERVER_PORT);
-            // server = new TSimpleServer(new TServer.Args(transport).processor(processor));
-            mul_sever = new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor));
+            TServerSocket transport = new TServerSocket(Global.DEFAULT_SERVER_PORT);
+            var server = new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor));
             logger.info("Starting ThssDB ...");
-            mul_sever.serve();
+            server.serve();
         } catch (TTransportException e) {
             logger.error(e.getMessage());
         }
